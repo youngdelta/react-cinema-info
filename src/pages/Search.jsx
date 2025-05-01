5e6a6b65148660e249678f9001cfa79abdddf527
iamai <youngdelta@gmail.com>
2025-05-01 16:16:15 +0900

    etc...

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,18 +1,27 @@
-import { useState } from 'react';
+import { useRef } from 'react';
 import { useQuery } from '@tanstack/react-query';
 import { searchMovies } from '../api/tmdb';
 import MovieCard from '../components/MovieCard';
 import Pagination from '../components/Pagination';
 import useHoverStore from '../store/useHoverStore';
+import useSearchStore from '../store/useSearchStore';
 
 function Search() {
-	const [searchQuery, setSearchQuery] = useState('');
+	// 검색어 상태를 전역 스토어에서 가져옴
+	const { searchQuery, setSearchQuery } = useSearchStore();
+	// 현재 페이지 번호와 전체 페이지 수를 설정하는 함수를 Zustand 스토어에서 가져옴
 	const { currentPage, setTotalPages } = useHoverStore();
 
+	const searchTxtRef = useRef(null);
+
 	const { data, isLoading, error, refetch } = useQuery({
+		// 검색 쿼리와 현재 페이지를 기반으로 한 고유 쿼리 키
 		queryKey: ['searchMovies', searchQuery, currentPage],
+		// TMDB API를 호출하여 영화 검색을 수행하는 함수
 		queryFn: () => searchMovies(searchQuery, currentPage),
+		// 자동 실행 비활성화 (수동으로 refetch 호출 필요)
 		enabled: false,
+		// 검색 성공 시 전체 페이지 수를 저장
 		onSuccess: (data) => {
 			setTotalPages(data.total_pages);
 		},
@@ -38,6 +47,7 @@ function Search() {
 				<input
 					type="text"
 					value={searchQuery}
+					ref={searchTxtRef}
 					onChange={(e) => setSearchQuery(e.target.value)}
 					placeholder="영화 제목을 입력하세요"
 					className="search-input"
